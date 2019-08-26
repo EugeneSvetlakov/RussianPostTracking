6d37628f6b858fc70c0919b4f6ae41793a30abe9
Eugene Svetlakov <eugene.svetlakov@gmail.com>
2019-08-26 23:22:52 +0300

    Разбираюсь почему пустой ответ

--- RussianPostTracking/Program.cs
+++ RussianPostTracking/Program.cs
@@ -3,15 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RussianPostTracking.ruPost;
 
 namespace RussianPostTracking
 {
-    public class PostalTracking { }
+    public class PostalTracking
+    {
+        AuthorizationHeader authorization { get; set; }
+        OperationHistoryRequest request { get; set; }
+
+        public PostalTracking()
+        {
+            
+            
+        }
+
+
+        
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            // http://www.compilemode.com/2016/03/calling-web-service-using-soap-request-without-wsdl-and-endpoints.html
+            // https://victorz.ru/20180805729
+            OperationHistory12 operation = new OperationHistory12();
+            AuthorizationHeader authorization = new AuthorizationHeader
+            {
+                mustUnderstand = "1",
+                login = "HXkJjcymqMqwBh",
+                password = "Ry65q1pxuWf6"
+            };
+            OperationHistoryRequest request = new OperationHistoryRequest
+            {
+                MessageType = 0,
+                Barcode = "22221312123123"
+            };
+            var res = operation.getOperationHistory(
+                request
+                , authorization
+                );
+            
+            
+            Console.Write(res.ToString());
+            Console.ReadKey();
         }
     }
 }
